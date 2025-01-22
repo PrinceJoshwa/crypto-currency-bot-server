3bdc0f52f3bc9ab768eafeced91251e66d44ae78
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-01-22 10:15:12 +0530

    first commit

--- src/server.js
+++ src/server.js
@@ -9,14 +9,11 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors({
-    origin: [
-      process.env.CLIENT_URL,
-      'https://crypto-currency-bot-client.vercel.app'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  }));
+    origin: 'https://crypto-currency-bot-client.vercel.app' 
+}));
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api/crypto", cryptoRoutes)
